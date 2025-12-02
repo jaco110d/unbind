afdb4eeb92503620189961742606eb6ed44cdd96
Jacob Hartmann <jacob.gade.hartmann@gmail.com>
2025-12-02 16:49:11 +0100

    Initial commit

---
+++ Unbind/UnbindApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnbindApp.swift
+//  Unbind
+//
+//  Created by Jacob Hartmann on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct UnbindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
